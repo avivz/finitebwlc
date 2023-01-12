a38ce8122ae7b33704177e187b5093733c2b0eef
Aviv Zohar <avivzohar@gmail.com>
2023-01-12 16:53:19 +0200

    update to non-slurm

--- experiment_honest_growth/run.py
+++ experiment_honest_growth/run.py
@@ -14,12 +14,12 @@ base_arguments = ["--time 1000","--num_honest 100", "--pow_honest 0.01", "--head
 
 
 
-bandwidth_range = numpy.arange(0.1,4.2,0.1)
+bandwidth_range = numpy.arange(0.1,4.2,1)
 for index, bandwidth in enumerate(bandwidth_range):
     file_name = os.path.join(OUTPUT_PATH, "exp1_" + str(index))
     arguments = base_arguments + [f"--bandwidth {bandwidth}", f"--saveResults {file_name}"]
     cmd = f"{PYTHON_PATH} {SIMULATION_PATH} {' '.join(arguments)}"
     sbatch_cmd = f'sbatch --mem=1g -c1 --wrap="{cmd}"'
-    print(f"{index}: running:\n\t{sbatch_cmd}")
-    os.system(sbatch_cmd)
+    print(f"{index}: running:\n\t{cmd}")
+    os.system(cmd)
 print(cmd)
