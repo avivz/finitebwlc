7d51285af8255245dcdc75cd5ccf2a1be2f24f0b
Aviv Zohar <avivzohar@gmail.com>
2023-01-14 11:15:35 +0200

    make queue stable

--- src/limitted_queue.py
+++ src/limitted_queue.py
@@ -6,7 +6,8 @@ PRIO_TYPE = TypeVar('PRIO_TYPE')
 
 class LimittedQueue(Generic[ITEM_TYPE, PRIO_TYPE]):
     """max priority queue that keeps only the top k items. 
-    Enqueue is O(n) where n is the actual queue size, dequeue is O(1)"""
+    Enqueue is O(n) where n is the actual queue size, dequeue is O(1)
+    If two equal priority items are placed inside the queue, the earlier one has higher priority"""
 
     def __init__(self, buffer_size: int = 10) -> None:
         # a list of recent items entered to the queue:
@@ -22,10 +23,11 @@ class LimittedQueue(Generic[ITEM_TYPE, PRIO_TYPE]):
         else:
             self._queue.append(rec)
             ind = len(self._queue)-2
-            while ind > -1:
-                if self._queue[ind][0] > self._queue[ind+1][0]:  # type: ignore
-                    self._queue[ind], self._queue[ind +
-                                                  1] = self._queue[ind+1], self._queue[ind]
+
+            while ind > -1 and \
+                    self._queue[ind][0] >= self._queue[ind+1][0]:  # type: ignore
+                self._queue[ind], self._queue[ind +
+                                              1] = self._queue[ind+1], self._queue[ind]
                 ind -= 1
 
     def dequeue(self) -> ITEM_TYPE:
