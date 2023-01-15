3bde50f6a15f3f98f19a9b8a36f2d8e9f42522da
Aviv Zohar <avivzohar@gmail.com>
2023-01-15 13:15:06 +0200

    repetitions + avoid repeating existing files

--- experiment_honest_growth/run.py
+++ experiment_honest_growth/run.py
@@ -28,32 +28,42 @@ base_arguments = ["--run_time 10000", "--num_honest 100",
                   "--pow_honest 0.01"]  # "--header_delay 0"  "--bandwidth -1"
 
 bandwidth_range = numpy.arange(0.1, 4.2, 0.05)
+num_repetitions = 10
 
 parser = setup_parser()
 args = parser.parse_args()
 
 for index, bandwidth in enumerate(bandwidth_range):
-    file_name1 = os.path.join(OUTPUT_PATH, "exp1_band_" + str(index)+".json")
-    arguments1 = base_arguments + \
-        [f"--header_delay 0 --bandwidth {bandwidth}",
-            f"--saveResults {file_name1}"]
+    for rep in range(num_repetitions):
+        file_name1 = os.path.join(
+            OUTPUT_PATH, "exp1_band_" + str(index)+"_"+str(rep)+".json")
+        arguments1 = base_arguments + \
+            [f"--header_delay 0 --bandwidth {bandwidth}",
+                f"--saveResults {file_name1}"]
 
-    file_name2 = os.path.join(OUTPUT_PATH, "exp1_delay_" + str(index)+".json")
-    arguments2 = base_arguments + \
-        [f"--header_delay {1/bandwidth} --bandwidth -1",
-            f"--saveResults {file_name2}"]
+        file_name2 = os.path.join(
+            OUTPUT_PATH, "exp1_delay_" + str(index)+"_"+str(rep)+".json")
+        arguments2 = base_arguments + \
+            [f"--header_delay {1/bandwidth} --bandwidth -1",
+                f"--saveResults {file_name2}"]
 
-    cmd1 = f"{PYTHON_PATH} {SIMULATION_PATH} {' '.join(arguments1)}"
-    cmd2 = f"{PYTHON_PATH} {SIMULATION_PATH} {' '.join(arguments2)}"
+        cmd1 = f"{PYTHON_PATH} {SIMULATION_PATH} {' '.join(arguments1)}"
+        cmd2 = f"{PYTHON_PATH} {SIMULATION_PATH} {' '.join(arguments2)}"
 
-    if args.slurm:
-        cmd1 = f'sbatch --mem=8g -c1 --wrap="{cmd1}"'
-        cmd2 = f'sbatch --mem=8g -c1 --wrap="{cmd2}"'
-        if args.no_out:
-            cmd1 += ' --output=/dev/null'
-            cmd2 += ' --output=/dev/null'
+        if args.slurm:
+            cmd1 = f'sbatch --mem=8g -c1 --wrap="{cmd1}"'
+            cmd2 = f'sbatch --mem=8g -c1 --wrap="{cmd2}"'
+            if args.no_out:
+                cmd1 += ' --output=/dev/null'
+                cmd2 += ' --output=/dev/null'
 
-    print(f"{index}: running:\n\t{cmd1}")
-    os.system(cmd1)
-    print(f"{index}: running:\n\t{cmd2}")
-    os.system(cmd2)
+        if os.path.exists(file_name1) and os.path.getsize(file_name1) > 0:
+            print(f"SKIPPING {file_name1}")
+        else:
+            print(f"{index}: RUNNING: {cmd1}")
+            os.system(cmd1)
+        if os.path.exists(file_name2) and os.path.getsize(file_name2) > 0:
+            print(f"SKIPPING {file_name2}")
+        else:
+            print(f"{index}: RUNNING: {cmd2}")
+            os.system(cmd2)
