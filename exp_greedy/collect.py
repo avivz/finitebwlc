b74c90a3a4a6b8c93634f87e5fb96366e6013135
Aviv Zohar <avivzohar@gmail.com>
2023-01-17 16:49:30 +0200

    added new experiment

---
+++ exp_greedy/collect.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+import json
+import os
+from typing import Dict, List, Tuple, Any
+import plotly.express as px  # type: ignore
+import plotly.graph_objects as go  # type: ignore
+import numpy as np
+import tqdm
+import csv
+import argparse
+
+BASE_PATH = os.path.split(os.path.abspath(__file__))[0]
+PYTHON_PATH = "python"
+BASE_DATA_PATH = os.path.join(BASE_PATH, "data/")
+
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='collect',
+        description='Analyzes results of the experiment',
+        fromfile_prefix_chars='@')
+
+    parser.add_argument('--logx',
+                        action='store_true', help="makes x axis logscale")  # on/off flag
+
+    parser.add_argument('--data_dir',
+                        nargs=1, help="where to find results within the data directory", required=True, type=str)
+
+    return parser
+
+
+parser = setup_parser()
+args = parser.parse_args()
+
+
+DATA_PATH = os.path.join(BASE_DATA_PATH, args.data_dir[0])
+BASE_OUT_PATH = os.path.join(BASE_PATH, "results/")
+if not os.path.exists(BASE_OUT_PATH):
+    os.mkdir(BASE_OUT_PATH)
+
+data: Dict[Tuple[float, str], Tuple[List[float], List[float]]] = dict()
+
+
+# read data from files
+print("Reading files...")
+for file_name in tqdm.tqdm(os.listdir(DATA_PATH)):
+    if not file_name.endswith(".json"):
+        continue
+    try:
+        with open(os.path.join(DATA_PATH, file_name), 'r') as file:
+            content = json.load(file)
+    except:
+        print("FAILED to read", file_name)
+
+    run_time = content["config"]["run_time"]
+    chain_height = content["honest_chain_height"]
+    ancestor_height = content["honest_chain_common_ancestor_height"]
+    bandwidth = content["config"]["bandwidth"]
+    download_rule = content["config"]["download_rule"]
+    if (bandwidth, download_rule) not in data:
+        data[(bandwidth, download_rule)] = [], []
+    data[(bandwidth, download_rule)][0].append(chain_height/run_time)
+    data[(bandwidth, download_rule)][1].append(ancestor_height/run_time)
+
+
+# compute the traces
+print("Computing traces...")
+
+records: List[Dict[str, Any]] = [
+    {
+        "bandwidth": key_record[0],
+        "download_rule":key_record[1],
+        "chain growth": sum(data[key_record][0]) / len(data[key_record][0]),
+        "stdev_cg": np.std(data[key_record][0]),
+        "max ancestor height": sum(data[key_record][1]) / len(data[key_record][1]),
+        "stdev_mah": np.std(data[key_record][1]),
+    } for key_record in sorted(list(data))
+]
+
+
+print("Max stdev chain growth:", max(
+    records,  key=(lambda x: x["stdev_cg"])))  # type: ignore
+print("Max stdev ancestor height:", max(
+    records,  key=(lambda x: x["stdev_mah"])))  # type: ignore
+
+print("Creating plot...")
+
+fig = px.scatter(records, x="bandwidth", y="max ancestor height",
+                 color="download_rule",
+                 labels={
+                     "x": "Bandwidth",
+                     "y": "Rate of chain growth",
+                 }, log_x=args.logx)
+
+# fig.add_trace(px.scatter(records, x="bandwidth", y="max ancestor height",
+#                          color="download_rule",
+#                          labels={
+#                              "x": "Bandwidth",
+#                              "y": "Rate of chain growth",
+#                          }, log_x=args.logx).data[0])
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.98,
+    xanchor="left",
+    x=0.02
+))
+
+out_path = os.path.join(BASE_OUT_PATH, args.data_dir[0])
+out_file = os.path.join(out_path, "fig_exp_greedy.svg")
+out_file2 = os.path.join(out_path, "fig_exp_greedy.png")
+
+
+print(f"Saving plot to {out_file}, {out_file2}")
+if not os.path.exists(out_path):
+    os.mkdir(out_path)
+fig.write_image(out_file)
+fig.write_image(out_file2)
+
+
+csv_file = os.path.join(out_path, "exp_greedy.csv")
+
+print(f"Saving csv to {csv_file}")
+
+with open(csv_file, 'w') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["x_bandwidth", "y_chain_growth",
+                        "y_ancestor_height", "download_rule"])
+    for record in records:
+        csv_writer.writerow(
+            [record["bandwidth"], record["chain growth"], record["max ancestor height"],
+             record["download_rule"]])
---
+++ exp_greedy/run.py
@@ -0,0 +1,101 @@
+import numpy
+import os
+import argparse
+from typing import List
+import tqdm
+import sim.configuration
+
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='run',
+        description='Runs the experiment',
+        fromfile_prefix_chars='@')
+
+    parser.add_argument('--data_dir',
+                        nargs=1, help="where to save results within the data directory", required=True, type=str)
+
+    parser.add_argument('--slurm',
+                        action='store_true', help="runs the code in parallel on slurm using sbatch")  # on/off flag
+    parser.add_argument('--no_out',
+                        action='store_true', help="runs the code in parallel on slurm using sbatch")  # on/off flag
+    return parser
+
+
+BASE_PATH = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+SIMULATION_MODULE = "sim.run_experiment"
+
+PYTHON_PATH = "python"
+
+DATA_ROOT_PATH = os.path.join(os.path.split(
+    os.path.abspath(__file__))[0], "data/")
+if not os.path.exists(DATA_ROOT_PATH):
+    os.mkdir(DATA_ROOT_PATH)
+
+parser = setup_parser()
+args = parser.parse_args()
+
+DATA_PATH = os.path.join(DATA_ROOT_PATH, args.data_dir[0])
+if not os.path.exists(DATA_PATH):
+    os.mkdir(DATA_PATH)
+
+base_arguments = [f"--{sim.configuration.RunConfig.RUN_TIME} 2000",
+                  f"--{sim.configuration.RunConfig.NUM_HONEST} 100",
+                  f"--{sim.configuration.RunConfig.MODE} pow",
+                  f"--{sim.configuration.RunConfig.HONEST_BLOCK_RATE} 0.01",
+                  f"--{sim.configuration.RunConfig.HEADER_DELAY} 0",
+                  f"--{sim.configuration.RunConfig.INDUCE_SPLIT} 0.001 10"
+                  ]
+
+
+bandwidth_range = numpy.arange(-2, 5.001, 0.2)
+bandwidth_range = numpy.power(10, bandwidth_range)
+
+num_repetitions = 5
+
+
+num_skipped = 0
+commands_to_run: List[str] = []
+
+for index, bandwidth in enumerate(bandwidth_range):
+    for rep in range(num_repetitions):
+        file_name1 = os.path.join(
+            DATA_PATH, "exp3_greedy_" + str(index)+"_"+str(rep)+".json")
+        arguments1 = base_arguments + [f"--{sim.configuration.RunConfig.BANDWIDTH} {bandwidth}",
+                                       f"--{sim.configuration.RunConfig.SAVE_RESULTS} {file_name1}",
+                                       f"--{sim.configuration.RunConfig.DOWNLOAD_RULE} {sim.configuration.DownloadRules.GreedyExtendChain.value}",]
+
+        file_name2 = os.path.join(
+            DATA_PATH, "exp3_longest_" + str(index)+"_"+str(rep)+".json")
+        arguments2 = base_arguments + [f"--{sim.configuration.RunConfig.BANDWIDTH} {bandwidth}",
+                                       f"--{sim.configuration.RunConfig.SAVE_RESULTS} {file_name2}",
+                                       f"--{sim.configuration.RunConfig.DOWNLOAD_RULE} {sim.configuration.DownloadRules.LongestHeaderChain.value}",]
+
+        cmd1 = f"{PYTHON_PATH} -m {SIMULATION_MODULE} {' '.join(arguments1)}"
+        cmd2 = f"{PYTHON_PATH} -m {SIMULATION_MODULE} {' '.join(arguments2)}"
+
+        if os.path.exists(file_name1) and os.path.getsize(file_name1) > 0:
+            print(f"SKIPPING {file_name1}")
+            num_skipped += 1
+        else:
+            commands_to_run.append(cmd1)
+        if os.path.exists(file_name2) and os.path.getsize(file_name2) > 0:
+            print(f"SKIPPING {file_name2}")
+            num_skipped += 1
+        else:
+            commands_to_run.append(cmd2)
+
+
+if args.slurm:
+    commands_to_run = [
+        f'sbatch --mem=8g -c1 --wrap="{cmd}"' for cmd in commands_to_run]
+    if args.no_out:
+        commands_to_run = [
+            cmd + ' --output=/dev/null' for cmd in commands_to_run]
+
+for command in tqdm.tqdm(commands_to_run):
+    print(f"RUNNING: {command}")
+
+    os.system(command)
+
+print(f"\n\nskipped: {num_skipped}, ran: {len(commands_to_run)}.")
