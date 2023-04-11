5d4b5826d9b0850746c2c8a8ec1d307bf5f42857
Joachim Neu <email@joachim-neu.de>
2023-04-11 12:48:19 -0700

    .

---
+++ exp_teaser_pos/collect.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+import json
+import os
+from typing import Dict, List, Tuple, Any
+import plotly.express as px  # type: ignore
+import plotly.graph_objects as go  # type:ignore
+import numpy as np
+import tqdm
+import csv
+import argparse
+
+BASE_PATH = os.path.split(os.path.abspath(__file__))[0]
+PYTHON_PATH = "python"
+BASE_DATA_PATH = os.path.join(BASE_PATH, "data/")
+
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='collect',
+        description='Analyzes results of the experiment',
+        fromfile_prefix_chars='@')
+
+    parser.add_argument('--logx',
+                        action='store_true', help="makes x axis logscale")  # on/off flag
+
+    parser.add_argument('--data_dir',
+                        nargs=1, help="where to find results within the data directory", required=True, type=str)
+
+    return parser
+
+
+parser = setup_parser()
+args = parser.parse_args()
+
+
+DATA_PATH = os.path.join(BASE_DATA_PATH, args.data_dir[0])
+BASE_OUT_PATH = os.path.join(BASE_PATH, "results/")
+if not os.path.exists(BASE_OUT_PATH):
+    os.mkdir(BASE_OUT_PATH)
+
+data: Dict[Tuple[float, bool], List[float]] = dict()
+
+
+# read data from files
+print("Reading files...")
+for file_name in tqdm.tqdm(os.listdir(DATA_PATH)):
+    if not file_name.endswith(".json"):
+        continue
+    try:
+        with open(os.path.join(DATA_PATH, file_name), 'r') as file:
+            content = json.load(file)
+    except:
+        print("FAILED to read", file_name)
+
+    run_time = content["config"]["run_time"]
+    chain_height = content["honest_chain_height"]
+    bandwidth = content["config"]["bandwidth"]
+    attacker = bool(content["config"]["teasing_attacker"] > 0)
+    if (bandwidth, attacker) not in data:
+        data[(bandwidth, attacker)] = []
+    data[(bandwidth, attacker)].append(chain_height/run_time)
+
+
+# compute the traces
+print("Computing traces...")
+
+records: List[Dict[str, Any]] = [
+    {
+        "bandwidth": record[0],
+        "attacker": "active attacker" if record[1] else "no attacker",
+        "chain growth": sum(data[record]) / len(data[record]),
+        "stdev": np.std(data[record])
+    } for record in sorted(list(data))
+]
+
+
+print("Max stdev:", max(records,  key=(lambda x: x["stdev"])))  # type: ignore
+
+print("Creating plot...")
+
+fig = px.scatter(records, x="bandwidth", y="chain growth",
+                 # error_y=bw_error_values+delay_error_values,
+                 color="attacker",
+                 labels={
+                     "x": "Bandwidth",
+                     "y": "Rate of chain growth",
+                 }, log_x=args.logx)
+
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.98,
+    xanchor="left",
+    x=0.02
+))
+
+out_path = os.path.join(BASE_OUT_PATH, args.data_dir[0])
+out_file = os.path.join(out_path, "fig_exp_teaser.svg")
+out_file2 = os.path.join(out_path, "fig_exp_teaser.png")
+
+
+print(f"Saving plot to {out_file}, {out_file2}")
+if not os.path.exists(out_path):
+    os.mkdir(out_path)
+fig.write_image(out_file)
+fig.write_image(out_file2)
+
+
+def write_to_csv(filename: str, fields: List[str], x_values: List[Any], y_values: List[Any], delimiter: str = ",") -> None:
+    print(f"Saving csv to {filename}")
+
+    with open(filename, 'w') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=delimiter)
+        csv_writer.writerow(fields)
+        for i in range(len(x_values)):
+            csv_writer.writerow(
+                [x_values[i], y_values[i]])
+
+
+write_to_csv(filename=os.path.join(out_path, "fig-experiment-teaser-noattacker-data.txt"),
+             fields=["bandwidth", "chain_growth"],
+             x_values=[record["bandwidth"]
+                       for record in records if record["attacker"] == "no attacker"],
+             y_values=[record["chain growth"]
+                       for record in records if record["attacker"] == "no attacker"],
+             delimiter=" "
+             )
+
+write_to_csv(filename=os.path.join(out_path, "fig-experiment-teaser-activeattacker-data.txt"),
+             fields=["bandwidth", "chain_growth"],
+             x_values=[record["bandwidth"]
+                       for record in records if record["attacker"] == "active attacker"],
+             y_values=[record["chain growth"]
+                       for record in records if record["attacker"] == "active attacker"],
+             delimiter=" "
+             )
---
+++ exp_teaser_pos/run.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+import numpy
+import os
+import argparse
+from typing import List
+import tqdm
+from p_tqdm import p_umap
+import sim.configuration
+
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='run',
+        description='Runs the experiment',
+        fromfile_prefix_chars='@')
+
+    parser.add_argument('--data_dir',
+                        nargs=1, help="where to save results within the data directory", required=True, type=str)
+
+    parser.add_argument('--slurm',
+                        action='store_true', help="runs the code in parallel on slurm using sbatch")  # on/off flag
+    parser.add_argument('--no_out',
+                        action='store_true', help="runs the code in parallel on slurm using sbatch")  # on/off flag
+    return parser
+
+
+BASE_PATH = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+SIMULATION_MODULE = "sim.run_experiment"
+
+PYTHON_PATH = "python"
+
+DATA_ROOT_PATH = os.path.join(os.path.split(
+    os.path.abspath(__file__))[0], "data/")
+if not os.path.exists(DATA_ROOT_PATH):
+    os.mkdir(DATA_ROOT_PATH)
+
+parser = setup_parser()
+args = parser.parse_args()
+
+DATA_PATH = os.path.join(DATA_ROOT_PATH, args.data_dir[0])
+if not os.path.exists(DATA_PATH):
+    os.mkdir(DATA_PATH)
+
+base_arguments = [f"--{sim.configuration.RunConfig.RUN_TIME} 10000", f"--{sim.configuration.RunConfig.NUM_HONEST} 100", f"--{sim.configuration.RunConfig.MODE} pow",
+                  f"--{sim.configuration.RunConfig.HONEST_BLOCK_RATE} 0.01", f"--{sim.configuration.RunConfig.HEADER_DELAY} 0"]
+
+bandwidth_range = numpy.arange(0.1, 2.001, 0.05)
+# bandwidth_range = numpy.arange(-2, 5.001, 0.05)
+# bandwidth_range = numpy.power(10, bandwidth_range)
+
+num_repetitions = 100
+
+
+num_skipped = 0
+commands_to_run: List[str] = []
+
+for rep in range(num_repetitions):
+    for index, bandwidth in enumerate(bandwidth_range):
+        file_name1 = os.path.join(
+            DATA_PATH, "exp2_band_" + str(index)+"_"+str(rep)+".json")
+        arguments1 = base_arguments + \
+            [f"--{sim.configuration.RunConfig.BANDWIDTH} {bandwidth}",
+                f"--{sim.configuration.RunConfig.SAVE_RESULTS} {file_name1}"]
+
+        file_name2 = os.path.join(
+            DATA_PATH, "exp2_teaser_" + str(index)+"_"+str(rep)+".json")
+        arguments2 = arguments1 + \
+            [f"--{sim.configuration.RunConfig.SAVE_RESULTS} {file_name2}",
+                f"--{sim.configuration.RunConfig.TEASING_ATTACKER} 1.0", f"--{sim.configuration.RunConfig.ATTACKER_HEAD_START} 100"]
+
+        cmd1 = f"{PYTHON_PATH} -m {SIMULATION_MODULE} {' '.join(arguments1)}"
+        cmd2 = f"{PYTHON_PATH} -m {SIMULATION_MODULE} {' '.join(arguments2)}"
+
+        # (we already have those values from exp_teaser)
+        # if os.path.exists(file_name1) and os.path.getsize(file_name1) > 0:
+        #     print(f"SKIPPING {file_name1}")
+        #     num_skipped += 1
+        # else:
+        #     commands_to_run.append(cmd1)
+        if os.path.exists(file_name2) and os.path.getsize(file_name2) > 0:
+            print(f"SKIPPING {file_name2}")
+            num_skipped += 1
+        else:
+            commands_to_run.append(cmd2)
+
+
+if args.slurm:
+    commands_to_run = [
+        f'sbatch --mem=8g -c1 --wrap="{cmd}"' for cmd in commands_to_run]
+    if args.no_out:
+        commands_to_run = [
+            cmd + ' --output=/dev/null' for cmd in commands_to_run]
+
+
+def run_command(command):
+    print(f"RUNNING: {command}")
+    os.system(command)
+
+
+p_umap(run_command, commands_to_run)
+
+
+print(f"\n\nskipped: {num_skipped}, ran: {len(commands_to_run)}.")
