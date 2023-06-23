e3795a0fceb253f65c34603f84d15e1ffa42a4de
Aviv Zohar <avivzohar@gmail.com>
2023-06-23 13:43:04 +0300

    generalize the teasing attacker to cases where it does not have a lead

--- sim/teasing_pow_attacker.py
+++ sim/teasing_pow_attacker.py
@@ -2,6 +2,7 @@
 from .block import Block
 from .node import Node
 import sim.network as network
+from typing import Optional
 
 
 class TeasingPoWAttacker(Node):
@@ -11,13 +12,22 @@ class TeasingPoWAttacker(Node):
 
     def __init__(self, genesis: Block, mining_rate: float, network: network.Network) -> None:
         super().__init__(genesis, mining_rate, bandwidth=0, header_delay=0, network=network)
+
+        # this is the tip of the chain this node is mining on:
         self._tip = genesis
+
         # this is the tip of the chain this node has allowed others to download.
         self._last_available = genesis
+
+        # This is the next block that this node will release (if it has one) for others to download:
+        self._next_available: Optional[Block] = None
 
     def mine_block(self) -> Block:
         block = super().mine_block()
         block.is_available = False
+
+        if block.parent == self._last_available:
+            self._next_available = block
 
         self._mining_target = block
         self._tip = block
@@ -25,19 +35,29 @@ class TeasingPoWAttacker(Node):
 
     def receive_header(self, block: Block) -> None:
         super().receive_header(block)
+
+        # Here the attacker will want to give up (if its tim is behind the honest chain):
+        if block.height > self._tip.height:
+            self._tip = block
+            self._mining_target = block
+            self._last_available = block
+            self._next_available = None
+            return
+
         self._mining_target = self._tip
 
+        # make blocks available up to the height of the honest chain:
         while block.height-1 > self._last_available.height:
-            children = self._last_available.get_children()
-            if not children:
-                return
-            self._last_available = children[0]
-            self._last_available.is_available = True
 
-        if self._tip.height >= self._last_available.height+2:
-            child = self._last_available.get_children()[0]
-            grandson = child.get_children()[0]
-            self._broadcast_header(grandson)
+            if not self._next_available:
+                return
+            self._last_available = self._next_available
+            self._last_available.is_available = True
+            if self._next_available.get_children():
+                self._next_available = self._next_available.get_children()[0]
+
+        if self._next_available and self._next_available.get_children():
+            self._broadcast_header(self._next_available.get_children()[0])
 
     def download_complete(self, block: Block) -> None:
         super().download_complete(block)
